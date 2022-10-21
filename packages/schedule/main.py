3c2b928bf4652d2843eb0786361f6a3f5b082387
Milinuri Nirvalen <pentergust@mail.ru>
2022-10-21 17:48:16 +0400

    Update: Chio plugin to SP v1.4

--- packages/schedule/main.py
+++ packages/schedule/main.py
@@ -1,6 +1,5 @@
 # ===========================
 from core import Plugin
-
 from tparser.tparser import ScheduledParser
 
 from datetime import datetime
@@ -8,17 +7,15 @@ from datetime import datetime
 
 p = Plugin('Расписание', desc='Отправляет вам расписание уроков')
 
-
 days = ["понедельник", "вторник", "сред", "четверг", "пятниц", "суббот"]
-s_days = ["Понедельник", "Вторник", "Среда", "четверг", "Пятницу", "Субботу"]
 
-@p.command("уроки", usage="[class_let] [day] уроки на день для класса")
+
+@p.command("уроки", usage="[class_let, day...] расписание на день")
 async def lessons(event, ctx):
-    sp = ScheduledParser(str(event.get('to.id')))
-
-    today = datetime.today().weekday()+1
-    class_let = sp.user["class_let"]
-
+    sp = ScheduledParser(str(event.get("to.id")))
+    class_let = None
+    days = []
+    
     for x in ctx.args:
         x = x.lower()
 
@@ -26,20 +23,31 @@ async def lessons(event, ctx):
             class_let = x
 
         for i, d in enumerate(days):
-            if x.startswith(d):
-                today = i 
-                continue
+            if x.startswith(d) and i not in days:
+                days.append(i) 
 
-        if x == 'сегодня':
-            today = datetime.today().weekday()
+        if x == "сегодня":
+            days.append(datetime.today().weekday())
 
-    if today > 5:
-        today = 0
-        
-    res = sp.get_lessons(today, class_let)
-    await ctx.message(res)
+    if not days:
+        days = datetime.today().weekday()
+
+    await ctx.message(sp.get_lessons(days, class_let))
+
+
+@p.command("расписание", usage= "[class_let] расписание на неделю")
+async def schedule(event, ctx):
+    sp = ScheduledParser(str(event.get("to.id")))
+    class_let = None
+
+    if ctx.sargs.lower() in sp.schedule["schedule"]:
+        class_let = sp.schedule["schedule"][ctx.sargs]
+
+    await ctx.message(sp.get_lessons([0, 1, 2, 3, 4, 5], class_let))
+
 
 @p.command('класс', usage='[класс] сменить класс по умолчанию')
 async def set_class(event, ctx):
     sp = ScheduledParser(str(event.get('to.id')))
     await ctx.message(sp.set_class(ctx.sargs.lower()))
+
