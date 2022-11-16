abf39c79a440b02f56c7b623fedb0074db14ca2c
Milinuri Nirvalen <pentergust@mail.ru>
2022-11-16 21:29:44 +0400

    Add: Schedule autopost

--- packages/schedule/main.py
+++ packages/schedule/main.py
@@ -6,7 +6,7 @@ Author: Milinuri Nirvalen
 Ver: sp 2.2
 """
 
-from core import Plugin
+from core import Plugin, Config
 from tparser.tparser import ScheduleParser
 
 from datetime import datetime
@@ -17,10 +17,99 @@ p = Plugin('Расписание', desc='Отправляет вам распи�
 days_str = ["понедельник", "вторник", "сред", "четверг", "пятниц", "суббот"]
 set_class_message = f"""\n\n⚠️ Поажлуйста, укажите класс по умолчанию: /класс [Ващ класс]"""
 
+config_path = "data/tparser_autopost.toml"
+user_base = {"autopost_hour":17, "hour":0, "day":0}
+
+
+@p.eventHandler('after')
+async def _(event, ctx):
+    p.log('Check tparser_autopost')
+
+    c = Config(filepath=config_path)
+    day = int(datetime.today().strftime('%j'))
+    hour = datetime.now().hour
+    updated = False
+    autopost_targets = []
+
+    # Проверка таймеров
+    # -----------------
+
+    for k, v in c.file_data.items():
+        if day != v["day"] and hour >= v["autopost_hour"]:
+            updated = True
+            autopost_targets.append(k)
+            v["day"] = day
+            v["hour"] = hour
+            c.file_data[k] = v
+
+    if updated:
+        c.save()
+        p.log("Save autopost file")
+
+
+    # Отправка расписания
+    # ===================
+
+    for x in autopost_targets:
+        sp = ScheduleParser(x)
+        await ctx.message(sp.print_today_lessons(), peer_id=x)
+
+# Настройка автопоста
+# ===================
+
+@p.command("автопост", usage="настроить автопост")
+async def autopost(event, ctx):
+    uid = str(event.get("to.id"))
+    c = Config(filepath=config_path)
+
+    res = "Автопост: "
+
+    if ctx.sargs.lower().startswith("выкл"):
+        if uid in c.file_data:
+            del c.file_data[uid]
+            c.save()
+    
+    elif ctx.sargs.isdigit():
+        user = c.file_data.get(uid, user_base)
+        user["autopost_hour"] = min(max(int(ctx.sargs), 0), 23)
+        c.file_data[uid] = user
+        c.save()
+
+    autopost_status = ""
+    autopost_command = ""
+
+    if uid not in c.file_data:
+        res += f'🔕 Выключен'
+        autopost_command = "\n🔷 /автопост [час] - включить автопост"
+    else:
+        res += f"🔔 В {c.file_data[uid]['autopost_hour']}:00"
+        autopost_command = "\n🔶 /автопост выкл - выключить автопост"
+
+    res += "\n\nВ указанное вами время вы получите расписание автоматически."
+    res += "\n⚠️ Уведомления приходят с запозданием (могут и не придти), не полагайтесь сильно на них."
+    res += autopost_command
+
+    await ctx.message(res)
+
+
+# Основные команды
+# ================
 
 @p.command("уроки", usage="[class_let; day] расписание на день")
 async def lessons(event, ctx):
-    sp = ScheduleParser(str(event.get("to.id")))
+    uid = str(event.get("to.id"))
+    sp = ScheduleParser(uid)
+    
+    # Обновление данных автопоста
+    # ---------------------------
+
+    c = Config(filepath=config_path)
+    if uid in c.file_data:
+        c.file_data[uid]["day"] = int(datetime.today().strftime('%j'))
+        c.file_data[uid]["hour"] = datetime.now().hour
+        c.save()
+
+
     class_let = None
     days = []
     
@@ -51,10 +140,20 @@ async def lessons(event, ctx):
 
     await ctx.message(res)
   
-
 @p.command("расписание", usage= "[class_let] расписание на неделю")
 async def schedule(event, ctx):
-    sp = ScheduleParser(str(event.get("to.id")))
+    uid = str(event.get("to.id"))
+    sp = ScheduleParser(uid)
+
+    # Обновление данных автопоста
+    # ---------------------------
+
+    c = Config(filepath=config_path)
+    if uid in c.file_data:
+        c.file_data[uid]["day"] = int(datetime.today().strftime('%j'))
+        c.file_data[uid]["hour"] = datetime.now().hour
+        c.save()
+
 
     res = sp.print_lessons([0, 1, 2, 3, 4, 5], ctx.sargs.lower())
 
@@ -63,18 +162,19 @@ async def schedule(event, ctx):
 
     await ctx.message(res)
 
-
 @p.command('класс', usage='[class_let] сменить класс по умолчанию')
 async def setClass(event, ctx):
     sp = ScheduleParser(str(event.get('to.id')))
     await ctx.message(sp.set_class(ctx.sargs.lower()))
 
 
+# Расширенные команды
+# ===================
+
 @p.command('tparser', usage='Статус парсера школьного расписания')
 async def tparserStatus(event, ctx):
     sp = ScheduleParser(str(event.get('to.id')))
     await ctx.message(sp.print_status())
-
 
 @p.command('<lesson(s) c(count)>', usage='[class_let] самые частые уроки')
 async def countLessons(event, ctx):
