f7d2092fc5ce5f22d133f838a5c06d476486f973
Milinuri Nirvalen <pentergust@mail.ru>
2023-04-01 17:06:13 +0400

    Update: spm_console to sp v5.2

--- spm_console.py
+++ spm_console.py
@@ -2,14 +2,21 @@
 Командный интерфейc для доступа к генератору сообщений.
 
 Author: Milinuri Nirvalen
-Ver: 1.0 (sp 5.0)
+Ver: 1.1 (sp 5.2)
 """
 
-from sp.filters import Filters
+from sp.counters import cl_counter
+from sp.counters import days_counter
+from sp.counters import group_counter_res
+from sp.counters import index_counter
+from sp.filters import construct_filters
+from sp.filters import parse_filters
 from sp.spm import SPMessages
 from sp.spm import send_update
+from sp.spm import send_counter
 
 import argparse
+
 from typing import Optional
 
 from loguru import logger
@@ -35,9 +42,13 @@ def get_parser() -> argparse.ArgumentParser:
                     help="Набор фильтров для уточнения результатов поиска")
 
     sp = subparsers.add_parser("counter", help="Счётчик уроков/кабинетов")
-    sp.add_argument("cl", nargs="?", default=None, help="Сортировка по классу")
-    sp.add_argument("-c", "--cabinets", help="Сортировака по кабинетам",
-                    action="store_true")
+    sp.add_argument("counter", help="Тип Счётчика", default="lessons",
+                    choices=["cl", "days", "lessons", "cabinets"])
+    sp.add_argument("target", help="Вторичный ключ для отображения",
+                    default=None,
+                    choices=["cl", "days", "lessons", "cabinets", "main"])
+    sp.add_argument("filters", nargs="*",
+                    help="Набор фильтров для уточнения расписания")
 
     sp = subparsers.add_parser("updates", help="Изменения в расписании")
     sp.add_argument("filters", nargs="*",
@@ -46,7 +57,6 @@ def get_parser() -> argparse.ArgumentParser:
 
 def main() -> None:
     sp = SPMessages("Console")
-    flt = Filters(sp.sc)
     parser = get_parser()
     args = parser.parse_args()
 
@@ -58,30 +68,47 @@ def main() -> None:
         logger.info("User class let = {}", args.cl)
 
     elif args.cmd == "sc":
-        flt.parse_args(args.filters)
+        flt = parse_filters(sp.sc, args.filters)
         if flt.days:
             print(sp.send_lessons(flt))
         else:
             print(sp.send_today_lessons(flt))
 
     elif args.cmd == "search":
-        flt.parse_args(args.filters)
+        flt = parse_filters(sp.sc, args.filters)
         if args.cabinets:
             print(sp.search_cabinet(args.target, flt))
         else:
             print(sp.search_lesson(args.target, flt))
 
     elif args.cmd == "counter":
-        print(sp.count_lessons(cabinets=args.cabinets, cl=args.cl))
+        flt = parse_filters(sp.sc, args.filters)
+        header = "✨ Счётчик"
+
+        if args.counter == "cl":
+            header += " по классам:"
+            res = cl_counter(sp.sc, flt)
+        elif args.counter == "days":
+            header += " по дням:"
+            res = days_counter(sp.sc, flt)
+        elif args.counter == "lessons":
+            header += " по урокам:"
+            res = index_counter(sp.sc, flt)
+        elif args.counter == "cabinets":
+            header += " по кабинетам:"
+            res = index_counter(sp.sc, flt, cabinets_mode=True)
+
+        groups = group_counter_res(res)
+        print(header)
+        print(send_counter(groups, target=args.target))
 
     elif args.cmd == "updates":
-        flt.parse_args(args.filters)
+        flt = parse_filters(sp.sc, args.filters)
         for u in sp.sc.get_updates(flt):
             print(send_update(u))
 
     else:
         print(sp.send_today_lessons(flt))
-
 
     if not sp.user["set_class"]:
         logger.warning("Please select your class!")
