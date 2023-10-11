e95b394b22b6ff3bb48752b233762016fa4d2ee2
Milinuri Nirvalen <pentergust@mail.ru>
2023-10-11 17:45:37 +0400

    Update: check_updates to telegram v1.14-b4

--- check_updates.py
+++ check_updates.py
@@ -8,30 +8,20 @@
 - Удаляет недействительных пользователей.
 
 Author: Milinuri Nirvalen
-Ver: 0.4 (sp 5.3, telegram 1.12)
+Ver: 0.5 (sp 6.0-b4, telegram 1.14-b4)
 """
 
-from sp.filters import construct_filters
-from sp.messages import SPMessages
-from sp.messages import send_update
-from sp.messages import users_path
-from sp.utils import load_file
-from sp.utils import save_file
-from telegram import bot
-from telegram import markup_generator
-from telegram import week_markup
-
-import asyncio
 from datetime import datetime
 from pathlib import Path
 
-from aiogram import Dispatcher
-from aiogram import executor
-from aiogram.utils.exceptions import BotKicked
-from aiogram.utils.exceptions import BotBlocked
-from aiogram.utils.exceptions import MigrateToChat
-
+from aiogram import Dispatcher, executor
+from aiogram.utils.exceptions import BotBlocked, BotKicked, MigrateToChat
 from loguru import logger
+
+from sp.intents import Intent
+from sp.messages import SPMessages, send_update, users_path
+from sp.utils import load_file, save_file
+from telegram import bot, markup_generator, week_markup
 
 
 dp = Dispatcher(bot)
@@ -42,19 +32,19 @@ CHAT_MIGRATE_MESSAGE = """⚠️ У вашего чата сменился ID.
 
 
 async def process_update(bot, hour: int, uid: str, user: dict) -> None:
-    """Проверяет обновления для пользователя (чата).
+    """Проверяет обновления для пользователя (или чата).
 
     Args:
-        bot (ТИП): Экземпляр aiogram бота
-        hour (int): Текущий час
-        uid (str): ID чата для проверки
-        user (dict): Данные пользователя
+        bot (bot): Экземпляр aiogram бота.
+        hour (int): Текущий час.
+        uid (str): ID чата для проверки.
+        user (dict): Данные пользователя.
     """
     sp = SPMessages(uid)
 
     # Отправляем расписание в указанные часы
     if str(hour) in user["hours"]:
-        message = sp.send_today_lessons(construct_filters(sp.sc))
+        message = sp.send_today_lessons(Intent.new())
         markup = markup_generator(sp, week_markup)
         await bot.send_message(uid, text=message, reply_markup=markup)
 
@@ -67,6 +57,10 @@ async def process_update(bot, hour: int, uid: str, user: dict) -> None:
 
         await bot.send_message(uid, text=message)
 
+
+# Главная функция скрипта
+# =======================
+
 async def main() -> None:
     hour = datetime.now().hour
     users = load_file(Path(users_path), {})
@@ -74,6 +68,7 @@ async def main() -> None:
     edited = False
 
     for k, v in list(users.items()):
+        # Если не включены уведомленияя или не укзаан класс
         if not v.get("notifications") or not v.get("class_let"):
             continue
 
@@ -81,23 +76,26 @@ async def main() -> None:
         try:
            await process_update(bot, hour, k, v)
 
+        # Если чат мигрировал в супергруппу
         except MigrateToChat as e:
             logger.info("Migrate to chat: {}", e)
             new_id = e.migrate_to_chat_id
-
             users[new_id] = users[k]
             del users[k]
             await bot.send_message(new_id, CHAT_MIGRATE_MESSAGE)
             edited = True
 
+        # Если бота заблокировали или исключили
         except (BotKicked, BotBlocked):
             logger.info("Remove user {}", k)
             edited = True
             del users[k]
 
+        # Все прочие исключения
         except Exception as e:
             logger.exception(e)
 
+    # Если данные изменились - записываем изменения
     if edited:
         logger.info("Save changed users file")
         save_file(Path(users_path), users)
