5a6916b862e43a24cfaa96d60bef013b7a1cf23e
Milinuri Nirvalen <pentergust@mail.ru>
2023-12-16 21:47:46 +0400

    CLI: Update to sp v5.7

--- spcli.py
+++ spcli.py
@@ -2,44 +2,34 @@
 Командный интерфейc для доступа к генератору сообщений.
 
 Author: Milinuri Nirvalen
-Ver: 1.3 (sp 5.5)
+Ver: 1.4 (sp 5.7)
 """
 
-from sp.counters import cl_counter
-from sp.counters import days_counter
-from sp.counters import group_counter_res
-from sp.counters import index_counter
-from sp.intents import Intent
-from sp.messages import SPMessages
-from sp.messages import send_update
-from sp.messages import send_counter
-from sp.messages import send_search_res
-
 import argparse
-
 from typing import Optional
 
-from loguru import logger
+from sp.counters import (cl_counter, days_counter, group_counter_res,
+                         index_counter)
+from sp.intents import Intent
+from sp.messages import SPMessages, send_counter, send_search_res, send_update
 
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", help="Информация о парсере",
+                        action="store_true")
+    parser.add_argument("--set-class", help="Изменить класс пользователя")
+    parser.add_argument("-i", "--intents", type=str,
+                        help="Набор намерений для генератора сообщений")
+
     subparsers = parser.add_subparsers(dest="cmd", metavar="command")
-    subparsers.add_parser("status", help="Информация о парсере")
-
-    sp = subparsers.add_parser("class", help="Изменить класс по умолчанию")
-    sp.add_argument("cl", help="Целевой класс по умолчанию")
-
     sp = subparsers.add_parser("sc", help="Расписание уроков")
-    sp.add_argument("filters", nargs="*",
-                    help="Набор фильтров для уточнения расписания")
+    sp = subparsers.add_parser("updates", help="Изменения в расписании")
 
     sp = subparsers.add_parser("search", help="Поиск в расписании")
     sp.add_argument("-c", "--cabinets", help="Поиск по кабинетам",
                     action="store_true")
     sp.add_argument("target", help="Цель для поиска: урок, кабинет")
-    sp.add_argument("filters", nargs="*",
-                    help="Набор фильтров для уточнения результатов поиска")
 
     sp = subparsers.add_parser("counter", help="Счётчик уроков/кабинетов")
     sp.add_argument("counter", help="Тип Счётчика", default="lessons",
@@ -47,12 +37,6 @@ def get_parser() -> argparse.ArgumentParser:
     sp.add_argument("target", help="Вторичный ключ для отображения",
                     default=None,
                     choices=["cl", "days", "lessons", "cabinets", "main"])
-    sp.add_argument("filters", nargs="*",
-                    help="Набор фильтров для уточнения расписания")
-
-    sp = subparsers.add_parser("updates", help="Изменения в расписании")
-    sp.add_argument("filters", nargs="*",
-                    help="Набор фильтров для уточнения расписания")
     return parser
 
 def main() -> None:
@@ -60,28 +44,48 @@ def main() -> None:
     parser = get_parser()
     args = parser.parse_args()
 
-    if args.cmd == "status":
+    if not sp.user["set_class"]:
+        print("[+] Please select your class!")
+        print("[?] Run: --set-class CLASS for set your class let")
+
+    # Статус генератора сообщений
+    if args.version:
         print(sp.send_status())
 
-    elif args.cmd == "class":
-        sp.set_class(args.cl)
-        logger.info("User class let = {}", args.cl)
+    # Смена класса пользователя
+    if args.set_class is not None:
+        status = sp.set_class(args.set_class)
+        if status:
+            print(f"[*] User class let = {args.set_class}")
+        else:
+            print(f"[*] Class let: {', '.join(sorted(sp.sc.lessons))}")
 
-    elif args.cmd == "sc":
-        intent = Intent.parse(sp.sc, args.filters)
+    # Получение намерений для расписания
+    if args.intents is not None:
+        intent = Intent.parse(sp.sc, args.intents.split())
+        print(intent)
+    else:
+        intent = Intent.new()
 
+    # Получить расписание уроков
+    if args.cmd == "sc":
         if intent.days:
             print(sp.send_lessons(intent))
         else:
             print(sp.send_today_lessons(intent))
 
+    # Просмотреть обновления в расписании
+    elif args.cmd == "updates":
+        for u in sp.sc.get_updates(intent):
+            print(send_update(u))
+
+    # Поиск в расписании
     elif args.cmd == "search":
-        intent = Intent.parse(sp.sc, args.filters)
         res = sp.sc.search(args.target, intent, args.cabinets)
         print(send_search_res(intent, res))
 
+    # Работа с счётчиками расписания
     elif args.cmd == "counter":
-        intent = Intent.parse(sp.sc, args.filters)
         header = "✨ Счётчик"
 
         if args.counter == "cl":
@@ -101,18 +105,8 @@ def main() -> None:
         print(header)
         print(send_counter(groups, target=args.target))
 
-    elif args.cmd == "updates":
-        intent = Intent.parse(sp.sc, args.filters)
-        for u in sp.sc.get_updates(intent):
-            print(send_update(u))
-
     else:
         print(sp.send_today_lessons(Intent.new()))
-
-    if not sp.user["set_class"]:
-        logger.warning("Please select your class!")
-        logger.warning("Run \"class --help\" for more information")
-
 
 # Запуск скрипта
 # ==============
