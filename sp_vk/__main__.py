43fa8e40f34e4f9a40472168d9aa56acd0a96fb6
Milinuri Nirvalen <pentergust@mail.ru>
2024-07-13 15:51:40 +0400

    Move sp_vk to new repository

--- sp_vk/__main__.py
+++
@@ -1,7 +0,0 @@
-from sp_vk.bot import bot
-from sp_vk.middleware import SpMiddleware
-
-
-if __name__ == "__main__":
-    bot.labeler.message_view.register_middleware(SpMiddleware)
-    bot.run_forever()
