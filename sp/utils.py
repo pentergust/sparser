963b3995ef141ea29328c678a15adf0a13154c67
Milinuri Nirvalen <pentergust@mail.ru>
2024-10-17 22:13:29 +0400

    Sp: Update utils docs

--- sp/utils.py
+++ sp/utils.py
@@ -3,14 +3,12 @@
 Используется как набор общих вспомогательных функций,
 которые работают внутри проекта, но редко выходят за его пределы.
 
-.. note:: Непостоянство
-
+Note: Непостоянство
     Обратите внимание что перечень функций не постоянный.
     Они могут быть со временем перемещены или удалены вовсе.
     Не стоит слишком сильно полагаться на них вне проекта sp.
 
 Содержит:
-
 - Функции для работы с json файлами.
 - Склонение слов относительно числа.
 - Получение строкового таймера обратного отсчёта.
@@ -18,7 +16,7 @@
 """
 
 from pathlib import Path
-from typing import Optional, Union
+from typing import Union
 
 # В теории более быстрый, чем стандартный json
 import ujson
@@ -27,23 +25,23 @@ from loguru import logger
 # Работа с json файлами
 # =====================
 
-def save_file(path: Path, data: Union[dict, list]):
+def save_file(path: Path, data: dict | list):
     """Записывает данные в json файл.
 
     Используется как обёртка для более удобной упаковки данных
     в json файлы.
     Автоматически создаёт файл, если он не найден.
 
-    .. deprecated:: 6.1
-        В скором времени проект откажется от использования json.
-        Данные методы будут перемещены.
+    Deprecated:
+        6.1: В скором времени проект откажется от использования json.
+            Данные методы будут удалены.
 
-    :param path: Путь к файлу для записи данных.
-    :type path: Path
-    :param data: Данные для записи в файл.
-    :type data: Union[dict, list]
-    :return: Ваши данные для записи.
-    :rtype: dict
+    Args:
+        path (Path): Путь к файлу для записи данных.
+        data (dict | list): Данные для записи в файл.
+
+    Returns:
+        dict: Ваши данные для записи.
     """
     logger.info("Write file {} ...", path)
     if not path.exists():
@@ -55,8 +53,8 @@ def save_file(path: Path, data: Union[dict, list]):
     return data
 
 def load_file(
-    path: Path, data: Optional[Union[dict, list]]=None
-) -> Union[dict, list]:
+    path: Path, data: dict | list | None=None
+) -> dict | list:
     """Читает данные из json файла.
 
     Используется как обёртка для более удобного чтения данных из
@@ -66,16 +64,16 @@ def load_file(
     Если файла не существует и данные переданы или возникло исключение
     при чтении файла -> возвращаем пустой словарь.
 
-    .. deprecated:: 6.1
-        В скором времени проект откажется от использования json.
+    Deprecated:
+        6.1: В скором времени проект откажется от использования json.
         Данные методы будут перемещены.
 
-    :param path: Путь к файлу для чтения.
-    :type path: Path
-    :param data: Данные для записи, по умолчанию не указаны.
-    :type data: Optional[dict], optional
-    :return: Распакованные данные из файла.
-    :rtype: Union[dict, list]
+    Args:
+        path (Path): Путь к файлу для чтения.
+        data (dict | list | None): Данные для записи, по умолчанию не указаны.
+
+    Returns:
+        dict | list: Распакованные данные из файла.
     """
     try:
         with open(path) as f:
@@ -102,34 +100,33 @@ def plural_form(n: int, v: tuple[str, str, str]) -> str:
     Возвращает склонённое слово: "для одного", "для двух",
     "для пяти" значений.
 
-    .. code-block:: python
-
-        plural_form(days, ("день", "дня", "дней"))
+    Examples:
+        >>> plural_form(days, ("день", "дня", "дней"))
         # days = 1 -> день
         # days = 32 -> дня
         # days = 65 -> дней
 
-    :param n: Некоторое число, используемое в склонении.
-    :type n: int
-    :param v: Варианты слова (для 1, для 2, для 5).
-    :type v: tuple[str]
-    :return: Склонённое слово в зависимости от числа.
-    :rtype: str
+    Args:
+        n (int): Некоторое число, используемое в склонении.
+        v (tuple[str, str, str]): Варианты слова (для 1, для 2, для 5).
+
+    Returns:
+        str: Склонённое слово в зависимости от числа.
     """
     return v[2 if (4 < n % 100 < 20) else (2, 0, 1, 1, 1, 2)[min(n % 10, 5)]] #noqa
 
-def get_str_timedelta(s: int, hours: Optional[bool]=True) -> str:
+def get_str_timedelta(s: int, hours: bool | None=True) -> str:
     """Возвращает строковый обратный отсчёт из количества секунд.
 
     Если hours = False -> ММ:SS.
     Если hours = True -> HH:MM:SS.
 
-    :param s: Количество секунд для обратного отсчёта.
-    :type s: int
-    :param hours: Использовать ли часы, по умолчанию да.
-    :type hours: Optional[bool], optional
-    :return: Строковый обратный отсчёт.
-    :rtype: str
+    Args:
+        s (int): Количество секунд для обратного отсчёта.
+        hours (bool | None): Использовать ли часы, по умолчанию да.
+
+    Returns:
+        str: Строковый обратный отсчёт.
     """
     if hours:
         h, r = divmod(s, 3600)
@@ -143,8 +140,8 @@ def get_str_timedelta(s: int, hours: Optional[bool]=True) -> str:
 # TODO: В надеждах когда-нибудь переписать эту страшную функцию
 # А пока работает и не трогаем, хе-хе ...
 def compact_updates( # noqa
-    updates: list[dict[str, Union[int, list[dict]]]]
-) -> dict[str, Union[int, list[dict]]]:
+    updates: list[dict[str, int | list[dict]]]
+) -> dict[str, int | list[dict]]:
     """Упаковывает несколько записей об обновлениях в одну.
 
     Используется чтобы совместить несколько записей об изменениях.
@@ -159,10 +156,13 @@ def compact_updates( # noqa
     - Если A -> B, B -> C, то A => C.
     - Иначе добавить запись.
 
-    :param updates: Список записей об обновлениях расписания.
-    :type updates: list[dict[str, Union[int, list[dict]]]]
-    :return: Новая упакованная запись об обновлённом расписании.
-    :rtype: dict[str, Union[int, list[dict]]]
+    Args:
+        updates (list[dict[str, int | list[dict]]]):
+            Список записей об обновлениях расписания.
+
+    Returns:
+        dict[str, int | list[dict]]:
+            Новая упакованная запись об обновлённом расписании.
     """
     res: dict[str, list[dict]] = updates[0]["updates"].copy()
 
