4907d3299f43140ddb9dbb3c3c6a34e099e78289
Milinuri Nirvalen <pentergust@mail.ru>
2025-09-02 20:34:33 +0400

    Server: Base app

---
+++ server/cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"splatorm/handlers"
+	"splatorm/provider"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func main() {
+	app := fiber.New()
+
+	// Расписание уроков
+	sc := handlers.ScheduleHandlers{
+		Provider: provider.DebugProvider{},
+	}
+	app.Get("/sc/today", sc.GetToday)
+	app.Get("/sc/week", sc.GetWeek)
+	app.Get("/sc/status", sc.GetStatus)
+	app.Get("/sc/:day", sc.GetDay)
+
+	// Общая информация
+	status := handlers.StatusHandlers{}
+	app.Get("/status", status.GetStatus)
+
+	app.Listen(":4096")
+}
