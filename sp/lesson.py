e73e71b81207d713c7c9abad78fcc860f682d2ea
Milinuri Nirvalen <pentergust@mail.ru>
2026-01-12 23:41:54 +0400

    Sp: Implement DayLessons and WeekLessons containers

--- sp/lesson.py
+++ sp/lesson.py
@@ -1,6 +1,6 @@
 """Представление Урока."""
 
-from collections.abc import Sequence
+from collections.abc import Iterator, Sequence
 from dataclasses import dataclass
 
 
@@ -79,3 +79,101 @@ class PartialLesson:
     def to_lesson(self, cl: str, day: int, order: int) -> Lesson:
         """Дополняет информацию до полноценного урока."""
         return Lesson(cl, self.name, self.cabinets, day, order)
+
+
+AnyLesson = Lesson | DayLesson | PartialLesson
+
+# Контейнеры для уроков
+# =====================
+
+
+class PartialSchedule:
+    """Представляет сжатое расписание."""
+
+    def __init__(self, day: int | None = None, cl: str | None = None) -> None:
+        self._day = day
+        self._cl = cl
+
+    def pack(self, lesson: AnyLesson) -> DayLesson | PartialLesson:
+        """Запаковывает урок для расписания.
+
+        - Partial -> Partial.
+        - Day -> Partial. Если совпадает класс.
+        - Lesson -> Day / Partial.
+        """
+        if isinstance(lesson, PartialLesson):
+            return lesson
+
+        if self._cl is not None and lesson.cl == self._cl:
+            return PartialLesson(lesson.name, lesson.cabinets)
+        return DayLesson(lesson.cl, lesson.name, lesson.cabinets)
+
+    def unpack(self, lesson: PartialLesson | DayLesson, order: int) -> Lesson:
+        """Дополняет информацию до полноценного урока."""
+        if isinstance(lesson, PartialLesson):
+            if self._cl is None:
+                raise ValueError("Class is bot specified")
+            return lesson.to_lesson(self._cl, self._day, order)
+        return lesson.to_lesson(self._day, order)
+
+
+class DayLessons(PartialSchedule):
+    """Уроки на день.
+
+    Хранит данные в сжатом виде.
+    Автоматически подставляет день недели и порядок урока.
+    Если указать класс, будет подставлять и его.
+    """
+
+    def __init__(
+        self,
+        lessons: Sequence[DayLesson | PartialLesson | None],
+        day: int,
+        cl: str | None = None,
+    ) -> None:
+        super().__init__(day, cl)
+        self.lessons = lessons or []
+
+    def lesson(self, order: int) -> Lesson | None:
+        """Возвращает полный урок по индексу."""
+        if order > len(self.lessons):
+            return None
+        dl = self.lessons[order]
+        if dl is None:
+            return None
+
+        return self.unpack(dl, order)
+
+    def iter_lessons(self) -> Iterator[Lesson | None]:
+        """Проходится по каждому уроку."""
+        for i, lesson in enumerate(self.lessons):
+            if lesson is None:
+                yield None
+                continue
+            yield self.unpack(lesson, i)
+
+
+class WeekLessons(PartialSchedule):
+    """Уроки на неделю.
+
+    Хранит данные в упрощённом виде.
+    Автоматически подставляет день недели и порядок урока.
+    Если указать класс, будет подставлять и его.
+    """
+
+    def __init__(
+        self,
+        lessons: Sequence[Sequence[DayLesson | PartialLesson | None]],
+        cl: str | None = None,
+    ) -> None:
+        super().__init__(cl=cl)
+        self.lessons = lessons
+
+    def day(self, day: int) -> DayLessons:
+        """Возвращает уроки на день."""
+        return DayLessons(self.lessons[day], day, self._cl)
+
+    def iter_days(self) -> Iterator[DayLessons]:
+        """Возвращает все дни в расписании."""
+        for day, lessons in enumerate(self.lessons):
+            yield DayLessons(lessons, day, self._cl)
